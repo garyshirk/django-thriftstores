3859f9d1c585aafa6ec65f74388083885014d1bb
Gary Shirk <garyshirk@yahoo.com>
2017-03-15 10:59:15 -0500

    #3 views.py now uses rest_framework generics for rest commands

--- thriftstores/views.py
+++ thriftstores/views.py
@@ -1,54 +1,13 @@
 from thriftstores.models import ThriftStore
 from thriftstores.serializers import ThriftStoreSerializer
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from thriftstores.permissions import IsAdminOrReadOnly
+from rest_framework import generics
 
 
-class ThriftStoreList(APIView):
-    """
-    List all thriftstores, or create a new thriftstore.
-    """
-    def get(self, request, format=None):
-        thriftstores = ThriftStore.objects.all()
-        serializer = ThriftStoreSerializer(thriftstores, many=True)
-        return Response(serializer.data)
+class ThriftStoreList(generics.ListCreateAPIView):
+    queryset = ThriftStore.objects.all()
+    serializer_class = ThriftStoreSerializer
 
-    def post(self, request, format=None):
-        serializer = ThriftStoreSerializer(data=request.data)
-        permission_classes = (IsAdminOrReadOnly, )
-        #print(request.data)
-        if serializer.is_valid() and permission_classes:
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class ThriftStoreDetail(APIView):
-    """
-    Retrieve, update or delete a thriftstore instance.
-    """
-    def get_object(self, pk):
-        try:
-            return ThriftStore.objects.get(pk=pk)
-        except ThriftStore.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        thriftstore = self.get_object(pk)
-        serializer = ThriftStoreSerializer(thriftstore)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        thriftstore = self.get_object(pk)
-        serializer = ThriftStoreSerializer(thriftstore, data=request.data)
-        if serializer.is_valid() and permission_classes:
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        thriftstore = self.get_object(pk)
-        thriftstore.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class ThriftStoreDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ThriftStore.objects.all()
+    serializer_class = ThriftStoreSerializer
